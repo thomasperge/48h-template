69c0a8aa869c7a9f35c53fe3a0c3a253fd53dc1f
manon <manon.lebarazergontier@ynov.com>
2023-04-13 16:07:02 +0200

    texte template

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func Home(rw http.ResponseWriter, r *http.Request) {
+	template, _ := template.ParseFiles("index.html")
 	fmt.Println(rw, r)
 	fmt.Println("TEST")
+
+	template.Execute(rw, r)
 }
 
 func main() {
@@ -15,5 +19,6 @@ func main() {
 		Home(rw, r)
 	})
 
-    fmt.Println("http://localhost:8080/")
+	fmt.Println("http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
 }
